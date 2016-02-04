32457865372369a9632fe99ecce7d46f912ffd0e
Darryl Nunn <darrylnu@gmail.com>
2016-02-03 23:01:29 -0500

    added basic navigation bar at top, added text field for user to enter city, button to fetch webpage data fetches unencoded data

--- Can I Wear Shorts Today?/ViewController.swift
+++ Can I Wear Shorts Today?/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var cityField: UITextField!
+    
+    @IBOutlet var weatherDisplayLabel: UILabel!
 
+    @IBAction func fetchWeather(sender: AnyObject) {
+        
+        print(cityField.text)
+        
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/\(cityField.text!)/forecasts/latest")!
+        
+        print(url)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if data != nil {
+                print(data)
+            }
+        
+        }
+        task.resume()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
